2f59d99ac04afa31bf315c6fb212827d5f66771d
ghostdevelop <ghost46@protonmail.com>
2017-01-06 20:37:37 -0500

    cookie popup fix

--- addons/cookie-popup/js/cookie-popup.js
+++ addons/cookie-popup/js/cookie-popup.js
@@ -3,7 +3,7 @@
     'use strict';
   
     if ( 'set' !== $.cookie( 'cookie-pop' ) ) {
-		$('<div class="cookie-popup">' + cookie_pop_text.message + '<a id="accept-cookies">' + cookie_pop_text.acceptLabel + '</a><a href="' + cookie_pop_text.moreUrl + '" id="cookies-more-info">' + cookie_pop_text.moreLabel + '</a></div>').appendTo('body')//.delay(8000).fadeOut();
+		$('<div class="cookie-popup"><p>' + cookie_pop_text.message + '</p><a id="accept-cookies">' + cookie_pop_text.acceptLabel + '</a><a href="' + cookie_pop_text.moreUrl + '" id="cookies-more-info">' + cookie_pop_text.moreLabel + '</a></div>').appendTo('body').delay(4000).fadeOut();
         $( '#accept-cookies' ).live('click', function () {
             $.cookie( 'cookie-pop', 'set', { expires : 10 });
             $( '.cookie-popup' ).remove();
